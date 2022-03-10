fef82a1b887c039904ab74952995e77e3c001a75
teppeiKiyoshi <martinemanuelleo@gmail.com>
2022-03-10 23:34:30 +0800

    accomplished homepage w/o functions

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//variables for nav header and side bar
+const body = document.querySelector('body'),
+  nav = document.querySelector('nav'),
+  searchToggle = document.querySelector('.searchToggle'),
+  sidebarOpen = document.querySelector('.sidebarOpen'),
+  siderbarClose = document.querySelector('.siderbarClose');
+
+// js code to toggle search box
+searchToggle.addEventListener('click', () => {
+  searchToggle.classList.toggle('active');
+});
+
+//   js code to toggle sidebar
+sidebarOpen.addEventListener('click', () => {
+  nav.classList.add('active');
+});
+
+body.addEventListener('click', (e) => {
+  let clickedElm = e.target;
+
+  if (
+    !clickedElm.classList.contains('sidebarOpen') &&
+    !clickedElm.classList.contains('menu')
+  ) {
+    nav.classList.remove('active');
+  }
+});
